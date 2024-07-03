44a8cf1a981b29c3d063203a2a1aa78824e2dc23
PJ Pjhayley Hayley <pjhayley@pjs-laptop.mynetworksettings.com>
2024-07-03 14:04:58 -0400

    inital commit

---
+++ src/components/Ping.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <button type="button" class="btn btn-primary">{{ msg }}</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Ping",
+  data() {
+    return {
+      msg: "Hello",
+    };
+  },
+  methods: {
+    async getMessage() {
+      const response = await axios.get("http://localhost:5000/ping");
+      this.msg = response.data;
+      console.log(response);
+    },
+  },
+  beforeMount() {
+    this.getMessage();
+  },
+};
+</script>
